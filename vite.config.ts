b12b499a5a0aa7732b629164b7a0dad21bd7b887
Angel Umeh <akcumeh@gmail.com>
2026-05-12 06:37:33 +0100

    Add pages & styling

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+    publicDir: resolve(__dirname, 'public'),
     build: {
+        outDir: resolve(__dirname, 'dist'),
+        emptyOutDir: true,
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
